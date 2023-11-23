d8a3790b6af836d3d46659ef64e4bf59aa7ebb2d
epe12345 <132982056+epe12345@users.noreply.github.com>
2023-11-23 14:28:42 +0900

    feat: UserDAO update 기능 추가, login 기능 sql문 수정, userController login, regist, update 기능 추가
    UserDAO update 기능 추가
    UserDAO login 기능 sql문 수정userController login, regist, update 기능 추가

--- src/main/java/persistance/UserDAO.java
+++ src/main/java/persistance/UserDAO.java
@@ -10,7 +10,7 @@ import domain.UserVO;
 public class UserDAO {
 	// 회원 가입할 때 insert할 로직
 	// FIXME 현재는 테스트를 위한 static 추가함
-	public static void insertUser(UserVO user) {
+	public int insertUser(UserVO user) {
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 
@@ -20,15 +20,19 @@ public class UserDAO {
 
 			pstmt = conn.prepareStatement(sql);
 			pstmt.setString(1, user.getId());
-			pstmt.setString(2, user.getUserName());
-			pstmt.setString(3, user.getMobile());
-			pstmt.setString(4, user.getEmail());
-			pstmt.setInt(5, user.getStudentId());
+			pstmt.setString(2, user.getPasswd());
+			pstmt.setString(3, user.getUserName());
+			pstmt.setString(4, user.getMobile());
+			pstmt.setString(5, user.getEmail());
+			pstmt.setInt(6, user.getStudentId());
 			pstmt.executeUpdate();
+
+			return 1;
 		}
 
 		catch (Exception ex) {
 			System.out.println("insertUser() 에러 : " + ex);
+			return -1;
 		}
 
 		finally {
@@ -45,7 +49,7 @@ public class UserDAO {
 
 	// ID로 회원 정보를 불러오는 메소드
 	// FIXME 현재는 테스트를 위한 static 추가함
-	public static UserVO getUser(String Id) {
+	public UserVO getUser(String Id) {
 		// DB 연결을 위한 초기 변수 선언
 		Connection conn = null;
 		PreparedStatement pstmt = null;
@@ -54,13 +58,15 @@ public class UserDAO {
 		String sql;
 
 		// SQL문 작성하기
-		sql = "select * from user where id = '" + Id + "' ";
+		sql = "select * from user where id = ?";
 
 		// 예외처리 시작, DB 연결 시작
 		try {
+
 			// 사용자 정보 가져옴
 			conn = DBConnection.getConnection();
 			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, Id);
 			rs = pstmt.executeQuery();
 
 			// 한개씩 가져옴
@@ -133,18 +139,101 @@ public class UserDAO {
 
 	// 정보 수정 할 때
 	// FIXME 현재는 테스트를 위한 static 추가함
-	public static void updateUser(String Id) {
+	// insert 참고
+	// parameter로 받은 user는 update할 새로운 user
+	// id가 같은 user를 찾고 그 user와 다른 값 update
+	public int updateUser(UserVO user) {
+		// DB 연결을 위한 초기 변수 선언
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
 
+		String id = user.getId();
+
+		String sql = "update user set passwd=?, userName=?, mobile=?, email=?, studentId=? where id=?";
+		// 예외처리 시작, DB 연결 시작
+		try {
+			conn = DBConnection.getConnection();
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, user.getPasswd());
+			pstmt.setString(2, user.getUserName());
+			pstmt.setString(3, user.getMobile());
+			pstmt.setString(4, user.getEmail());
+			pstmt.setInt(5, user.getStudentId());
+			pstmt.setString(6, id);
+			pstmt.executeUpdate();
+			// 수정 성공
+			return 1;
+
+		} catch (Exception ex) {
+			System.out.println("getListCount() 에러: " + ex);
+			// 수정 실패
+			return -1;
+		} finally {
+			try {
+				if (rs != null)
+					rs.close();
+				if (pstmt != null)
+					pstmt.close();
+				if (conn != null)
+					conn.close();
+			} catch (Exception ex) {
+				throw new RuntimeException(ex.getMessage());
+			}
+		}
+	}
+
+	// 로그인
+	public int login(String id, String passwd) {
+		// DB 연결을 위한 초기 변수 선언
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+
+		try {
+			String sql = "select passwd from user where id = ?";
+			conn = DBConnection.getConnection();
+
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, id);
+			rs = pstmt.executeQuery();
+			if (rs.next()) {
+				if (rs.getString(1).equals(passwd)) {
+					return 1; // 비밀번호 일치
+				} else {
+					return 0; // 비밀번호 불일치
+				}
+			}
+			return -1; // 아이디가 없음
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return -2; // DB 오류
 	}
 
 	// 회원 탈퇴할 때 DB의 내용을 지우는 메소드 -> 필요할까?
 
 	// 테스트 하려면 main에서 메소드들 호출
 	public static void main(String[] args) {
+		/*
 		String id = "jjj";
+		String passwd = "1324";
+		String userName = "하하하";
+		String mobile = "010-7654-8753";
+		String email = "sdjfad@tukorea.ac.kr";
+		int studentId = 123415621;
 		System.out.println("현재 DB에 있는 갯수 : " + getListCount());
-
-		UserVO user = getUser(id);
-		user.printLog();
+		UserVO user = new UserVO();
+		user.setId(id);
+		user.setPasswd(passwd);
+		user.setUserName(userName);
+		user.setMobile(mobile);
+		user.setEmail(email);
+		user.setStudentId(studentId);
+		
+		updateUser(user);
+		*/
+		// UserVO user = getUser(id);
+		// user.printLog();
 	}
 }
